4b2d5b9d62f4efcedab4b65d2da943ed1d0e68b0
AbdullahMouzeen <abdullah.mouazeen@gmail.com>
2021-01-20 22:59:57 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
 import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Header() {
+    
+    return (<header className = "App" > Welcome To Odometar </header>);
+}
+// function FoodChoice1(props) {
+//   console.log(props);
+//   const food = "Shawerma";
+//   return (
+//     <div className="food">
+//       <h2>Food Choice 1 for today is</h2>
+//       <h3>{props.favFood}</h3>
+//     </div>
+//   );
+// }
+class Odoclass extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            odo: "0000",
+        };
+    }
+    changeodo(n) {
+        let num = Number(this.state.odo) + n;
+        if (num > 9999) {
+            this.setState({ odo: "0000" });
+            return;
+        }
+        if (num < 10) {
+            this.setState({ odo: "000" + num });
+            return;
+        }
+        if (num < 100) {
+            this.setState({ odo: "00" + num });
+            return;
+        }
+        if (num < 1000) {
+            this.setState({ odo: "0" + num });
+            return;
+        }
+        this.setState({ odo: num });
+    }
+    render() {
+        return ( <>
+            < button onClick = {() => this.changeodo(1) } > add 1</button> 
+            < button onClick = {() => this.changeodo(10) } > add 10 </button> 
+            <button onClick = { () => this.changeodo(100) } > add 100 </button> 
+            <button onClick = { () => this.changeodo(1000) } > add 1000 </button> 
+            < div className = "odo" >
+            < h3 > { this.state.odo } </h3> 
+            </div> 
+            </>
+        );
+    }
 }
 
-export default App;
+// function Main() {
+//   return (
+//     <main>
+//       <div className="App">
+//         {/* <h1>Hello {courseName}</h1> */}
+//         <h1></h1>
+//         <p></p>
+//       </div>
+//     </main>
+//   );
+// }
+
+function Footer() {
+    return ( <footer className = "App" >
+        <p> Copy Right
+        for </p> 
+        <span > < a href = "#" > uniodo.com </a> </span >
+        </footer>
+    );
+}
+
+function App() {
+    return ( <>
+        <Header /> { /* <Main /> */ } 
+        <Odoclass />
+        <Footer />
+        </>
+    );
+}
+export default App
